145b367ef46456912ff5c66d51bb0245bd5ab60e
Alexander Soudry <soudryalex@gmail.com>
2023-12-17 17:43:56 -0500

    added some base components for the website

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+// components/Home.js
+import React from 'react';
+import '../component.css';
+
+const Contact = () => {
+  return (
+    <div id="contact" className="section">
+      <h2>Contact</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id eu nisl nunc mi ipsum faucibus. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Blandit volutpat maecenas volutpat blandit. Sit amet est placerat in egestas erat. Molestie nunc non blandit massa. Justo laoreet sit amet cursus sit amet dictum. Aliquam faucibus purus in massa tempor nec feugiat. In nibh mauris cursus mattis molestie a iaculis at. Arcu non sodales neque sodales ut etiam sit. Netus et malesuada fames ac turpis egestas integer eget aliquet. Vestibulum mattis ullamcorper velit sed. Urna duis convallis convallis tellus id interdum. Justo donec enim diam vulputate ut pharetra sit amet aliquam. Dictum non consectetur a erat.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id eu nisl nunc mi ipsum faucibus. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Blandit volutpat maecenas volutpat blandit. Sit amet est placerat in egestas erat. Molestie nunc non blandit massa. Justo laoreet sit amet cursus sit amet dictum. Aliquam faucibus purus in massa tempor nec feugiat. In nibh mauris cursus mattis molestie a iaculis at. Arcu non sodales neque sodales ut etiam sit. Netus et malesuada fames ac turpis egestas integer eget aliquet. Vestibulum mattis ullamcorper velit sed. Urna duis convallis convallis tellus id interdum. Justo donec enim diam vulputate ut pharetra sit amet aliquam. Dictum non consectetur a erat.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id eu nisl nunc mi ipsum faucibus. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Blandit volutpat maecenas volutpat blandit. Sit amet est placerat in egestas erat. Molestie nunc non blandit massa. Justo laoreet sit amet cursus sit amet dictum. Aliquam faucibus purus in massa tempor nec feugiat. In nibh mauris cursus mattis molestie a iaculis at. Arcu non sodales neque sodales ut etiam sit. Netus et malesuada fames ac turpis egestas integer eget aliquet. Vestibulum mattis ullamcorper velit sed. Urna duis convallis convallis tellus id interdum. Justo donec enim diam vulputate ut pharetra sit amet aliquam. Dictum non consectetur a erat.</p>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../component.css';
+
+const Home = () => {
+  return (
+    <div id="home" className="section" >
+      <h1>Hello Welcome to my World</h1>
+      <h1> I'm the Full stack Developer that you need.</h1>
+      <h1> Scroll down to learn more!!!</h1>
+    </div>
+  );
+};
+
+export default Home;
